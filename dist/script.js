0be4554ce1de075c3a8f168c6ed85bfcc7a2bf20
volp99 <stefanovolpatti@yahoo.it>
2021-04-11 23:49:56 +0200

    rename folder dist in src

--- dist/script.js
+++
@@ -1,9 +0,0 @@
-/*    Hamburger menu    */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
